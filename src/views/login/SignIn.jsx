c55eb4a752e58448e39ad6b10720c5894c42c944
Justin W Soto <soto@pdx.edu>
2022-01-11 14:38:36 -0800

    reworking api routes to use diff api

--- src/views/login/SignIn.jsx
+++ src/views/login/SignIn.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function SignIn() {
+export default function SignIn(onClick) {
+
+ 
     return (
         <div>
             <h2>sign in</h2>
+        
         </div>
     )
 }
--- src/views/search/CampFinder.jsx
+++ src/views/search/CampFinder.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function CampFinder() {
+    const [ setSearchResults] = useState([])
+
+    useEffect(() => {
+        const getResults = async () => {
+            const apiSearchResults = await fetchResults()
+            setSearchResults(apiSearchResults)
+        }
+        getResults()
+    }, [])
+
+    const fetchResults = async () => {
+        const res = await fetch ( "some env thing");
+         const data = await res.json()
+            console.log(data)
+
+    }
+
     return (
         <div>
             <h1>Search campgrounds ya dingus</h1>
+
         </div>
     )
 }
