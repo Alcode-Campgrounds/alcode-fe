14199fd080899ead27fd6a64287570a2a112b523
Justin W Soto <soto@pdx.edu>
2022-01-10 18:06:47 -0800

    added form functionality and tried to fetch that biscuit

--- src/views/login/SignUp.jsx
+++ src/views/login/SignUp.jsx
@@ -1,17 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
+import { newUserSignUp } from '../../utils/AuthFetch';
 
 export default function SignUp({ user = false }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmitSignUp = async (e) => {
+        e.preventDefault()
+           if(!user) {
+                   const newUser = await newUserSignUp({ email, password, name })
+                   console.log(newUser);
+                
+        }
+    }
+
     return (
-        
-            <fieldset className='form-main'>
+            <fieldset>
             <legend>{ user ? 'SignIn' : 'SignUp'}</legend>
                 <br />
-                <input type="text" placeholder='email' className='input'/>
-                <input type="text" placeholder='password' className='input'/>
-                { !user && <input type="text" placeholder='name' className='input'/>}
+            <form className='form-main' onSubmit={handleSubmitSignUp}>
+                <label htmlFor="email-input">
+                <input id="email-input"  required  value={email} type="text" placeholder='email' className='input' onChange={({target}) => {
+                    setEmail(target.value);
+                }}/>
+                </label>
+                <label htmlFor="password-input">
+                <input id="password-input" required  value={password} type="text" placeholder='password' className='input' onChange={({target}) => {
+                    setPassword(target.value);
+                }}/>
+                </label>
+
+                { !user && 
+                <label htmlFor="name-input">
+                <input id="name-input" required value={name} type="text" placeholder='name' className='input' onChange={({target}) => {
+                    setName(target.value);
+                }}/>
+                </label>}
                 <br />
                 <button type='submit' className='btn-main'>{ user ? 'SignIn' : 'SignUp'}</button>
                 <br />
+            </form>
             </fieldset>
         
     )
