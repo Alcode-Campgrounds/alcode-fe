c32fa0b147d0ddf80e0ffa152c43117eedf5faaf
satyadvaya <satyadvaya@gmail.com>
2022-01-11 16:02:19 -0800

    created Campsite and CampsiteList components

--- src/views/search/CampFinder.jsx
+++ src/views/search/CampFinder.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+// import useFetch from "react-fetch-hook";
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+};
 
 export default function CampFinder() {
-    return (
-        <div>
-            <h1>Search campgrounds ya dingus</h1>
-        </div>
-    )
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const getResults = async () => {
+      const apiSearchResults = await fetchResults();
+      setSearchResults(apiSearchResults);
+    };
+    getResults();
+  }, []);
+
+  const fetchResults = async () => {
+    const res = await fetch(process.env.REACT_APP_BACKEND_URL);
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h1>Search campgrounds ya dingus</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          id="search"
+          type="text"
+          name="search"
+          value={search}
+          //   onChange={handleFormChange}
+          required
+        />
+        <button type="submit" aria-label="Search">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
