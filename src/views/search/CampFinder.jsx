db0840571a8bddc2fd4e39aace9c1dd38d0dc94b
TANNER MECK <steve.meck246@gmail.com>
2022-01-13 11:43:53 -0800

    removed unneeded useState react hook

--- src/views/search/CampFinder.jsx
+++ src/views/search/CampFinder.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useUser } from "../../context/UserContext";
 import { fetchAllFacilities } from "../../utils/campFetch";
 
