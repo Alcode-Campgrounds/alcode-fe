7c89dfc219980e1f9a96ec5434da3b4c2002c318
Justin W Soto <soto@pdx.edu>
2022-01-14 15:53:59 -0800

    re formatted test for signup

--- src/views/login/SignUp.test.jsx
+++ src/views/login/SignUp.test.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { create }from 'react-test-renderer'
 import { UserProvider } from '../../context/UserContext'
 import SignUp from './SignUp'
-import { MemoryRouter, Routes, Route } from 'react-router-dom'
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom'
+// import { render } from '@testing-library/react'
+ 
 
  
 
@@ -13,14 +15,15 @@ describe('is it here?', () => {
     
     const wrapper = create(
     <UserProvider>
-        <MemoryRouter initialEntries={["/signup"]}>
-        <Routes>
-           <Route path="signup" element={<SignUp />} />
-        </Routes>
-        </MemoryRouter>
+        <Router initialEntries={["/signup"]}>
+            <Routes>
+                <Route path="signup" element={<SignUp />} />
+            </Routes>
+        </Router>
     </UserProvider>
     )
 
         expect(wrapper).toMatchSnapshot()
     })
 })
+
