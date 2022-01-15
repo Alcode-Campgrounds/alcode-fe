41617b398d9a8959c2f3e8bd8cbf6817767b41f5
Justin W Soto <soto@pdx.edu>
2022-01-14 16:03:23 -0800

    fixed fieldset issue

--- src/views/login/SignUp.jsx
+++ src/views/login/SignUp.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useUser } from "../../context/UserContext";
-import { existingUserSignIn, newUserSignUp } from "../../utils/AuthFetch";
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
+import { existingUserSignIn, newUserSignUp } from '../../utils/AuthFetch';
 
 export default function SignUp({ hasUser = false }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [error, setError] = useState("");
-  const {setUser} = useUser()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [error, setError] = useState('');
+  const { setUser } = useUser();
 
   let navigate = useNavigate();
 
@@ -17,20 +17,20 @@ export default function SignUp({ hasUser = false }) {
     e.preventDefault();
 
     if (hasUser) {
-     const userData = await existingUserSignIn(email, password)
-     if (userData.email){
-      setUser({name: userData.name, email: userData.email})
-      navigate("/search");
-    } else {
-      setError('Wrong email/password')
-    }
+      const userData = await existingUserSignIn(email, password);
+      if (userData.email) {
+        setUser({ name: userData.name, email: userData.email });
+        navigate('/search');
+      } else {
+        setError('Wrong email/password');
+      }
     } else {
       try {
         const userSignUp = await newUserSignUp(name, email, password);
-        console.log(userSignUp.status)
-        if (userSignUp){
-          setUser({name,email});
-          navigate("/search");
+        console.log(userSignUp.status);
+        if (userSignUp) {
+          setUser({ name, email });
+          navigate('/search');
         }
       } catch (error) {
         setError(error.message);
@@ -39,32 +39,33 @@ export default function SignUp({ hasUser = false }) {
   };
 
   return (
-    <fieldset>
-      <legend>{hasUser ? "SignIn" : "SignUp"}</legend>
-      <br />
-      <form className="form-main" onSubmit={handleSubmitSignUp}>
-      <a href={process.env.REACT_APP_OAUTH}>Sign in with your Google account</a>
-        <label htmlFor="email-input">
+    <form className='form-main' onSubmit={handleSubmitSignUp}>
+      <fieldset>
+        <legend>{hasUser ? 'SignIn' : 'SignUp'}</legend>
+        <a href={process.env.REACT_APP_OAUTH}>
+          Sign in with your Google account
+        </a>
+        <label htmlFor='email-input'>
           <input
-            id="email-input"
+            id='email-input'
             required
             value={email}
-            type="text"
-            placeholder="email"
-            className="input"
+            type='text'
+            placeholder='email'
+            className='input'
             onChange={({ target }) => {
               setEmail(target.value);
             }}
           />
         </label>
-        <label htmlFor="password-input">
+        <label htmlFor='password-input'>
           <input
-            id="password-input"
+            id='password-input'
             required
             value={password}
-            type="text"
-            placeholder="password"
-            className="input"
+            type='text'
+            placeholder='password'
+            className='input'
             onChange={({ target }) => {
               setPassword(target.value);
             }}
@@ -72,27 +73,25 @@ export default function SignUp({ hasUser = false }) {
         </label>
 
         {!hasUser && (
-          <label htmlFor="name-input">
+          <label htmlFor='name-input'>
             <input
-              id="name-input"
+              id='name-input'
               required
               value={name}
-              type="text"
-              placeholder="name"
-              className="input"
+              type='text'
+              placeholder='name'
+              className='input'
               onChange={({ target }) => {
                 setName(target.value);
               }}
             />
           </label>
         )}
-        <br />
-        <button type="submit" className="btn-main">
-          {hasUser ? "SignIn" : "SignUp"}
+        <button type='submit' className='btn-main'>
+          {hasUser ? 'SignIn' : 'SignUp'}
         </button>
-        <br />
         <p>{error}</p>
-      </form>
-    </fieldset>
+      </fieldset>
+    </form>
   );
 }
