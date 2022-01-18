3b47af77b49369fd2dfe226c1fe390fcca6fb953
Justin W Soto <soto@pdx.edu>
2022-01-17 19:56:17 -0800

    call to action btn links to signup page

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
@@ -7,6 +8,10 @@ export default function Home() {
             <div  className='hero-img' style={{ backgroundImage: 'url(../hero.jpg)'}}></div>
             <section className='hero-txt'>YOUR GO TO APP WHEN YOU WANT TO GET OUTSIDE</section>
             <section className='hero-subtxt'>Over 100,000 campsites to choose from on your next big adventure</section>
+            
+            <Link to="/signup">
+            <button className='hero-btn'>Lets go!</button>
+            </Link>
             </div>
         </>
         )
