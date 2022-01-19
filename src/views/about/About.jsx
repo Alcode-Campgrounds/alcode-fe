5de0cb3f941caaa1cc93e32d06deff544ef01aa6
Justin W Soto <soto@pdx.edu>
2022-01-19 08:55:47 -0800

    bio layout with cards complete, need bio info with links to github, li etc

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -6,22 +6,23 @@ export default function About() {
        <> 
             <h1 className='about-heading'>Our Devs</h1>
             <div className='about-container'>
+
                 <div className='about-list'>
                     <section className='alan-sect'>
                     <div className='alan'></div>
-                    <span className='dev-bio'>this is some text</span>
+                    <div className='dev-bio'>this is some textContrary to popular belief</div>
                     </section>
                     <section className='cristian-sect'>
                     <div className='cristian'></div>
-                    <span className='dev-bio'>this is some text</span>
+                    <div className='dev-bio'>this is some text</div>
                     </section>
                     <section className='tanner-sect'>
                     <div className='tanner'></div>
-                    <span className='dev-bio'>this is some text</span>
+                    <div className='dev-bio'>this is some text</div>
                     </section>
                     <section className='justin-sect'>
                     <div className='justin'></div>
-                    <span className='dev-bio'>this is some text</span>
+                    <div className='dev-bio'>this is some text</div>
                     </section>
                 </div>
             </div>
