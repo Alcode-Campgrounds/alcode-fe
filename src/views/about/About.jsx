ff4ad26e5805f4c9412e8755b74c71b4d61ad095
Justin W Soto <soto@pdx.edu>
2022-01-19 12:04:34 -0800

    added social links to about view

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -10,19 +10,32 @@ export default function About() {
                 <div className='about-list'>
                     <section className='alan-sect'>
                     <div className='alan'></div>
-                    <div className='dev-bio'>this is some textContrary to popular belief</div>
+                    <div className='dev-bio'>
+                        <a className='social-links' href="https://github.com/satyadvaya">github</a>
+                        <a className='social-links' href="https://www.linkedin.com/in/alan-willoughby/">linked in</a>
+                        <a className='social-links' href="mailto:satyadvaya@gmail.com?subject=Inquiry" >Email me</a>
+                    </div>
                     </section>
                     <section className='cristian-sect'>
                     <div className='cristian'></div>
-                    <div className='dev-bio'>this is some text</div>
+                    <div className='dev-bio'>
+                        <a className='social-links' href="https://github.com/cristi className='social-links'n-montes">github</a>
+                        <a className='social-links' href="https://www.linkedin.com/in/cristianmontesmujica/">linked in</a>
+                        <a className='social-links' href="mailto:cmontesmujica@gmail.com?subject=Inquiry" >Email me</a></div>
                     </section>
                     <section className='tanner-sect'>
                     <div className='tanner'></div>
-                    <div className='dev-bio'>this is some text</div>
+                    <div className='dev-bio'>
+                        <a className='social-links' href="https://github.com/tannermeck">github</a>
+                        <a className='social-links' href="https://www.linkedin.com/in/tanner-meck/">linked in</a>
+                        <a className='social-links' href="mailto:t.meck@students.clark.edu?subject=Inquiry" >Email me</a></div>
                     </section>
                     <section className='justin-sect'>
                     <div className='justin'></div>
-                    <div className='dev-bio'>this is some text</div>
+                    <div className='dev-bio'>
+                        <a className='social-links' href="https://github.com/justin-w-soto">github</a>
+                        <a className='social-links' href="https://www.linkedin.com/in/justinwsoto/">linked in</a>
+                        <a className='social-links' href="mailto:justinwsoto@gmail.com?subject=Inquiry" >Email me</a></div>
                     </section>
                 </div>
             </div>
