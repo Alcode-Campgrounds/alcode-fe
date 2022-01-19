474bbf11845dd51035649e9573448fabfc0009b4
Justin W Soto <soto@pdx.edu>
2022-01-18 21:33:56 -0800

    done with about for tonight, will add bios tommorrow

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -9,19 +9,19 @@ export default function About() {
                 <div className='about-list'>
                     <section className='alan-sect'>
                     <div className='alan'></div>
-                    <p className='dev-bio'>this is some text</p>
+                    <span className='dev-bio'>this is some text</span>
                     </section>
                     <section className='cristian-sect'>
                     <div className='cristian'></div>
-                    <p className='dev-bio'>this is some text</p>
+                    <span className='dev-bio'>this is some text</span>
                     </section>
                     <section className='tanner-sect'>
                     <div className='tanner'></div>
-                    <p className='dev-bio'>this is some text</p>
+                    <span className='dev-bio'>this is some text</span>
                     </section>
                     <section className='justin-sect'>
                     <div className='justin'></div>
-                    <p className='dev-bio'>this is some text</p>
+                    <span className='dev-bio'>this is some text</span>
                     </section>
                 </div>
             </div>
